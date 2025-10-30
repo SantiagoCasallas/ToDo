e6c1fda2ffe31edd7d7fa5eb2cfb49cdff4e925e
Santiago C <dscasallasr@udistrital.edu.co>
2025-10-30 11:29:37 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto1/Program.cs
@@ -0,0 +1,21 @@
+﻿var bob = new Sale("alo");
+bob.print(bob.getX());
+
+class Sale
+{
+    string x { get; set; }
+
+    public Sale(string message)
+    {
+        x = message;
+    }
+
+    public String getX()
+    {
+        return x;
+    }
+    public void print(string x)
+    {
+        Console.WriteLine("del void "+x);
+    }
+}
